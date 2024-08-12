5672e2378ac6a971a038c9ba7718ae58e461addf
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-08-13 05:08:47 +0800

    home
    home

---
+++ skibidi/lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

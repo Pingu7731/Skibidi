46170782544a382f5ddafdbf002bbb3f8d1f7a68
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2024-08-16 08:53:59 +0800

    Box animation
    -added inset box shadow pubspec
    -added box animation
    -added  dark mode

--- skibidi/lib/home.dart
+++ skibidi/lib/home.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
 
 class homepage extends StatefulWidget {
   const homepage({super.key});
@@ -10,20 +9,47 @@ class homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<homepage> {
+  bool pressed = true;
+  bool dark = true;
+
   @override
   Widget build(BuildContext context) {
-    const backgrouncolor = Color(0xFFE7ECEF);
+    final backgrouncolor =
+        dark ? const Color(0xFF2E3239) : const Color(0xFFE7ECEF);
+    double blur = pressed ? 5.0 : 30.0;
+    Offset distance = pressed ? const Offset(10, 10) : const Offset(28, 28);
+
     return Scaffold(
       backgroundColor: backgrouncolor,
       body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: Colors.black,
-          ),
-          child: const SizedBox(
-            height: 200,
-            width: 200,
+        child: Listener(
+          onPointerUp: (_) => setState(() => pressed = false),
+          onPointerDown: (_) => setState(() => pressed = true),
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 50),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: backgrouncolor,
+              boxShadow: [
+                BoxShadow(
+                  offset: -distance,
+                  color: dark ? const Color(0xFF35393F) : Colors.white,
+                  blurRadius: blur,
+                  inset: pressed,
+                ),
+                BoxShadow(
+                  offset: distance,
+                  color:
+                      dark ? const Color(0xFF23262A) : const Color(0xFFA7A9AF),
+                  blurRadius: blur,
+                  inset: pressed,
+                )
+              ],
+            ),
+            child: const SizedBox(
+              height: 200,
+              width: 200,
+            ),
           ),
         ),
       ),
